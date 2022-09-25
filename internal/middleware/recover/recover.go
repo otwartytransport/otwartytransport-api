1a55f7cd1599514542c8be0aa461a443310a56eb
Adam Zambrzycki <adam@zambrzycki.net>
2022-09-25 13:16:33 +0200

    Use fiber constant for error status code

--- internal/middleware/recover/recover.go
+++ internal/middleware/recover/recover.go
@@ -6,17 +6,18 @@ import (
 )
 
 func New() fiber.Handler {
-	return func(c *fiber.Ctx) (err error) {
+	return func(ctx *fiber.Ctx) (err error) {
 		defer func() {
 			if r := recover(); r != nil {
 				var ok bool
 				if err, ok = r.(error); !ok {
-					c.Status(500)
-					err = c.JSON(errors.Unknown)
+					err = ctx.
+						Status(fiber.StatusInternalServerError).
+						JSON(errors.Unknown)
 				}
 			}
 		}()
 
-		return c.Next()
+		return ctx.Next()
 	}
 }
