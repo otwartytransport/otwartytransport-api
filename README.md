3c577cb2c272a6531d18333be77e46bb15c5fee4
NozderkaJakub <hatrix5555@gmail.com>
2022-09-28 00:37:59 +0200

    Load env to struct and add README.md

---
+++ README.md
@@ -0,0 +1,9 @@
+# otwartytransport-api
+
+## Run
+
+To run the application specify environment using `APP_ENV` (possible: `dev`, `prod`).
+
+```
+APP_ENV=dev go run cmd/server.go
+```
--- cmd/server.go
+++ cmd/server.go
@@ -1,11 +1,40 @@
 package main
 
 import (
-	"github.com/otwartytransport/otwartytransport-api/internal/server"
+	"fmt"
 	"log"
+	"os"
+
+	"github.com/caarlos0/env/v6"
+	"github.com/joho/godotenv"
+	"github.com/otwartytransport/otwartytransport-api/internal/server"
 )
 
+type Config struct {
+	AppPort int `env:"APP_PORT,notEmpty,unset"`
+}
+
+func loadConfig(environment string) (Config, error) {
+	var cfg Config
+	dotenv := fmt.Sprintf(".env.%s", environment)
+
+	if err := godotenv.Load(dotenv); err != nil {
+		return cfg, err
+	}
+
+	if err := env.Parse(&cfg); err != nil {
+		return cfg, err
+	}
+
+	return cfg, nil
+}
+
 func main() {
+	cfg, err := loadConfig(os.Getenv("APP_ENV"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	srv := server.NewServer()
-	log.Fatal(srv.Listen(":8010"))
+	log.Fatal(srv.Listen(fmt.Sprintf(":%d", cfg.AppPort)))
 }
